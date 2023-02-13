9976ce4dd52c19ac56357d10632d5272e3160fc2
unknown <12cmorrison12@gmail.com>
2023-02-13 16:59:18 -0700

    finished problems 12, 13, 14

--- 12_multiply_even_numbers/multiply_even_numbers.py
+++ 12_multiply_even_numbers/multiply_even_numbers.py
@@ -12,3 +12,11 @@ def multiply_even_numbers(nums):
         >>> multiply_even_numbers([1, 3, 5])
         1
     """
+
+    for num in nums:
+        if num % 2 == 0:
+            product = product * num
+        else: 
+            product == 1
+
+    return product
